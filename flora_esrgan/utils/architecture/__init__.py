efc21a8946102f4026064100c4198e241c173798
barretto <b4rretto@mail.com>
2024-10-31 16:28:15 -0400

    Add initial versioning and restructure package layout

---
+++ flora_esrgan/utils/architecture/__init__.py
@@ -0,0 +1,4 @@
+from block import *
+from RRDB import *
+from SPSR import *
+from SRVGG import *
---
+++ setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+import os
+import subprocess
+
+default_version = '0.1.0'
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
+def get_git_hash():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        sha = out.strip().decode('ascii')
+    except OSError:
+        sha = 'unknown'
+
+    return sha
+
+
+def get_hash():
+    if os.path.exists('.git'):
+        sha = get_git_hash()[:7]
+    else:
+        sha = 'unknown'
+
+    return sha
+
+
+def get_requirements(filename='requirements.txt'):
+    here = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(here, filename), 'r') as f:
+        requires = [line.replace('\n', '') for line in f.readlines()]
+    return requires
+
+
+if __name__ == '__main__':
+    setup(
+        name='flora_esrgan',
+        version=default_version,
+        description='Enhanced Super-Resolution Generative Adversarial Networks (ESRGAN)',
+        long_description=readme(),
+        long_description_content_type='text/markdown',
+        author='Francisco Barretto',
+        author_email='francisco@florafauna.ai',
+        keywords='computer vision, pytorch, image restoration, super-resolution, esrgan',
+        url='https://github.com/fbarretto/ESRGAN',
+        include_package_data=True,
+        packages=find_packages(exclude=('tests', 'docs')),
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.10',
+        ],
+        license='MIT',
+        setup_requires=['cython', 'numpy'],
+        install_requires=get_requirements(),
+        python_requires='>=3.10',
+        zip_safe=False
+    )
